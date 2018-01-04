030b58fbf6fcae37a32418e9ad0dce7bf7a92983
Rafael Silvestre <rafaelsilvestre.183@gmail.com>
2018-01-04 15:11:08 -0200

    first commit

---
+++ stayapp-stamps.php
@@ -0,0 +1,8 @@
+<?php
+    /*
+        Plugin Name: StayApp Stamps
+        Author: StaypApp
+        Description: Plugin para integrar as vendas efetuadas pelo woocommerce no StayApp
+        Version: 1.0
+        Author URI: http://stapapp.com.br
+    */
